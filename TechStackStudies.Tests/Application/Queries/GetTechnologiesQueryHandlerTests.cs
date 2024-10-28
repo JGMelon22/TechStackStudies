e4b52516ed35e55d564e04e2721e649bfe6e9493
JGMelon22 <jgvasconcellosm22@outlook.com>
2024-10-27 21:26:53 -0300

    tests: implement queries tests

---
+++ TechStackStudies.Tests/Application/Queries/GetTechnologiesQueryHandlerTests.cs
@@ -0,0 +1,74 @@
+using FluentAssertions;
+using Moq;
+using TechStackStudies.Application.Handlers;
+using TechStackStudies.Application.Queries;
+using TechStackStudies.DTOs;
+using TechStackStudies.Interfaces;
+using TechStackStudies.Models;
+using TechStackStudies.Models.Enums;
+
+namespace TechStackStudies.Tests.Application.Queries;
+
+public class GetTechnologiesQueryHandlerTests
+{
+    [Fact]
+    public async Task Should_ReturnListOfTechnologies_When_ThereIsResults()
+    {
+        // Arrange
+        Mock<ITechnologyRepository> technologyRepository = new Mock<ITechnologyRepository>();
+        GetTechnologiesQuery query = new GetTechnologiesQuery();
+        GetTechnologiesQueryHandler handler = new GetTechnologiesQueryHandler(technologyRepository.Object);
+        IEnumerable<TechnologyResponse> technologies = new List<TechnologyResponse>
+        {
+            new ()
+            {
+                Id = 1,
+                Name = ".NET",
+                IsFrameworkOrLib = true,
+                CurrentVersion = 9.0F,
+                Category = Models.Enums.Category.Backend,
+                SkillLevel = Models.Enums.SkillLevel.Skilled
+            },
+            new ()
+            {
+                Id = 2,
+                Name = "SQL Server",
+                IsFrameworkOrLib = false,
+                CurrentVersion = 2022,
+                Category = Models.Enums.Category.Database,
+                SkillLevel = Models.Enums.SkillLevel.Skilled
+            },
+            new ()
+            {
+                Id = 3,
+                Name = "Docker",
+                IsFrameworkOrLib = false,
+                CurrentVersion = 27.3F,
+                Category = Category.Devops,
+                SkillLevel = SkillLevel.Beginner
+            }
+        };
+        ServiceResponse<IEnumerable<TechnologyResponse>> serviceResponse = new ServiceResponse<IEnumerable<TechnologyResponse>>
+        {
+            Data = technologies,
+            Success = true,
+            Message = string.Empty
+        };
+
+        technologyRepository
+            .Setup(x => x.GetAllTechnologiesAsync())
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        ServiceResponse<IEnumerable<TechnologyResponse>> result = await handler.Handle(query);
+
+        // Assert
+        result.Data.Should().NotBeNull();
+        result.Data.Should().NotBeEmpty();
+        result.Data!.Count().Should().Be(3);
+        result.Success.Should().BeTrue();
+        result.Message.Should().Be(string.Empty);
+
+        technologyRepository.Verify(x => x.GetAllTechnologiesAsync(), Times.Once);
+    }
+}
---
+++ TechStackStudies.Tests/Application/Queries/GetTechnologyByIdQueryHandlerTets.cs
@@ -0,0 +1,58 @@
+using FluentAssertions;
+using Moq;
+using TechStackStudies.Application.Handlers;
+using TechStackStudies.Application.Queries;
+using TechStackStudies.DTOs;
+using TechStackStudies.Interfaces;
+using TechStackStudies.Models;
+using TechStackStudies.Models.Enums;
+
+namespace TechStackStudies.Tests.Application.Queries;
+
+public class GetTechnologyByIdQueryHandlerTests
+{
+    [Fact]
+    public async Task Should_ReturnSingleTechnology_When_ThereIsResult()
+    {
+        // Arrange
+        Mock<ITechnologyRepository> technologyRepository = new Mock<ITechnologyRepository>();
+        GetTechnologyByIdQuery query = new GetTechnologyByIdQuery(1);
+        GetTechnologyByIdQueryHandler handler = new GetTechnologyByIdQueryHandler(technologyRepository.Object);
+        TechnologyResponse technology = new TechnologyResponse()
+        {
+            Id = 1,
+            Name = "Vue.js",
+            IsFrameworkOrLib = true,
+            CurrentVersion = 3.5F,
+            Category = Category.Frontend,
+            SkillLevel = SkillLevel.Beginner
+        };
+
+        ServiceResponse<TechnologyResponse> serviceResponse = new ServiceResponse<TechnologyResponse>
+        {
+            Data = technology,
+            Success = true,
+            Message = string.Empty
+        };
+
+        technologyRepository
+            .Setup(x => x.GetTechnologyByIdAsync(1))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        ServiceResponse<TechnologyResponse> result = await handler.Handle(query);
+
+        // Assert
+        result.Data.Should().NotBeNull();
+        result.Success.Should().BeTrue();
+        result.Message.Should().Be(string.Empty);
+        result.Data!.Id.Should().Be(1);
+        result.Data!.Name.Should().Be("Vue.js");
+        result.Data!.IsFrameworkOrLib.Should().Be(true);
+        result.Data!.CurrentVersion.Should().Be(3.5F);
+        result.Data!.Category.Should().Be(Category.Frontend);
+        result.Data!.SkillLevel.Should().Be(SkillLevel.Beginner);
+
+        technologyRepository.Verify(x => x.GetTechnologyByIdAsync(1), Times.Once);
+    }
+}
