1a741541a48e4590b689719ae8b7ff65a73391bb
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-07-06 12:25:18 -0300

    refactor: ignore Id property on Mapperly Map instead of ignore compile time warning

--- TechStackStudies/Infrastructure/Mappers/TechnologyMapper.cs
+++ TechStackStudies/Infrastructure/Mappers/TechnologyMapper.cs
@@ -7,7 +7,9 @@ namespace TechStackStudies.Infrastructure.Mappers;
 [Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName)]
 public static partial class TechnologyMapper
 {
+    [MapperIgnoreTarget(nameof(Technology.Id))]
     public static partial Technology ToDomain(TechnologyRequest technology);
+    
     public static partial TechnologyResponse ToResponse(Technology technology);
     public static partial IEnumerable<TechnologyResponse> ToResponse(IEnumerable<Technology> technology);
 }
